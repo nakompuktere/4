d2d40be81b6bb9ccbadd5d51b12531d5c2e793ff
nakompuktere <144929164+nakompuktere@users.noreply.github.com>
2024-10-16 16:32:22 +0300

    Split books by two columns.

--- render_website.py
+++ render_website.py
@@ -1,34 +1,31 @@
 import json
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from livereload import Server, shell
+from livereload import Server
+from more_itertools import chunked
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html'])
-)
-template = env.get_template('template.html')
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html'])
+    )
+    template = env.get_template('template.html')
 
-def main():
     with open("books_description.json", "r", encoding="utf8") as my_file:
         books_json = my_file.read()
 
     books = json.loads(books_json)
-
+    chunked_books = list(chunked(books, 2))
     rendered_page = template.render(
-        books_parameters=books
+        books=chunked_books
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
-main()
+on_reload()
 
 server = Server()
 
-server.watch('template.html', main())
+server.watch('template.html', on_reload)
 
 server.serve(root='.')
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
